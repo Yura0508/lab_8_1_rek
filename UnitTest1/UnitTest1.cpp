738712b84d81b3180fd0431033303fecf538c77f
Yura <urijkisil2007@gmail.com>
2024-10-27 20:17:43 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_1_rek/lab_8_1_rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestCount)
+        {
+            char testStr1[] = "This is a while test while and another while.";
+            Assert::AreEqual(3, Count(testStr1));
+
+            char testStr2[] = "No matches here.";
+            Assert::AreEqual(0, Count(testStr2));
+
+            char testStr3[] = "while";
+            Assert::AreEqual(1, Count(testStr3));
+        }
+        // Тестуємо функцію Change
+        TEST_METHOD(TestChange)
+        {
+            char testStr1[] = "This is a while test.";
+            char expectedOutput1[] = "This is a *** test.";
+            char* dest1 = new char[101];
+            dest1[0] = '\0';
+            Change(dest1, testStr1, dest1, 0);
+            Assert::AreEqual(expectedOutput1, dest1);
+            delete[] dest1;
+
+            char testStr2[] = "No while here.";
+            char expectedOutput2[] = "No *** here.";
+            char* dest2 = new char[101];
+            dest2[0] = '\0';
+            Change(dest2, testStr2, dest2, 0);
+            Assert::AreEqual(expectedOutput2, dest2);
+            delete[] dest2;
+        }
+
+	};
+}
---
+++ lab_8_1_rek/lab_8_1_rek.cpp
@@ -0,0 +1,62 @@
+﻿// lab_8_1_rek.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+
+int Count(char* str, int i = 0) {
+	if (str[i + 4] != 0) {
+		if (str[i] == 'w' && str[i + 1] == 'h' && str[i + 2] == 'i' && str[i + 3] == 'l' && str[i + 4] == 'e') {
+			return 1 + Count(str, i + 5);
+		}
+		else {
+			return Count(str, i + 1);
+		}
+	}
+	else {
+		return 0;
+	}
+}
+
+
+char* Change(char* dest, const char* str, char* t, int i) {
+	if (str[i + 4] != 0) {
+		if (str[i] == 'w' && str[i + 1] == 'h' && str[i + 2] == 'i' && str[i + 3] == 'l' && str[i + 4] == 'e') {
+			strcat(t, "***");
+			return Change(dest, str, t + 3, i + 5);
+		}
+		else {
+			*t++ = str[i++];
+			*t = '\0';
+			return Change(dest, str, t, i);
+		}
+	}
+	else {
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t++ = str[i++];
+		*t = '\0';
+		return dest;
+	}
+}
+
+int main() {
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+
+	cout << "String contained " << Count(str) << " occurrences of 'while'" << endl;
+
+	char* dest1 = new char[101];
+	dest1[0] = '\0';
+	char* dest2 = Change(dest1, str, dest1, 0);
+
+	cout << "Modified string (param): " << dest1 << endl;
+	cout << "Modified string (result): " << dest2 << endl;
+
+	cin.get();
+	return 0;
+}
